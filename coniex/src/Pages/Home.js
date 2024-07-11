f8aa96977d219e17bbae18750a63ea3c0bcf0cb8
samratmonga <samratmonga.gni@gmail.com>
2024-07-11 16:59:40 +0530

    new code

--- coniex/src/Pages/Home.js
+++ coniex/src/Pages/Home.js
@@ -7,10 +7,23 @@ import { CountUp } from 'countup.js';
 import time from '../Assests/Images/time.png';
 import tableImg from '../Assests/Images/tableimg.png';
 import cryptoImg from '../Assests/Images/cryptoImg.png';
-import { East } from '@mui/icons-material';
-import {Link , NavLink} from 'react-router-dom';
+import { Earbuds, East } from '@mui/icons-material';
+import { Link, NavLink } from 'react-router-dom';
 import { siteName } from '../Config/Config';
-
+import productImg from '../Assests/Images/productImg.png';
+import safe from '../Assests/Images/safe.png';
+import security1 from '../Assests/Images/security.png';
+import Security2 from '../Assests/Images/setupbox.png';
+import security3 from '../Assests/Images/box.png'
+import trading from '../Assests/Images/trading.png';
+import qrcode from '../Assests/Images/qr code.png';
+import appstore from '../Assests/Images/apple-logo.png'
+import playstore from '../Assests/Images/play-button (1).png'
+import api from '../Assests/Images/appstore.png';
+import android from '../Assests/Images/android.png';
+import Accordion from 'react-bootstrap/Accordion';
+import { Slider } from '@mui/material';
+import ImageSlider from '../Components/ImageSlider';
 export default function Home() {
 
     // for counter 
@@ -46,7 +59,9 @@ export default function Home() {
     return (
         <>
             { /* ----------------- banner section start  ----------------- */}
+            <div className="banner-section-main-div">
             <div className='main-home'>
+                
                 <Container>
                     <Row>
                         <Col lg={6}>
@@ -69,6 +84,7 @@ export default function Home() {
                                     </form>
                                 </div>
                             </div>
+
 
                         </Col>
                         <Col lg={6}>
@@ -136,6 +152,11 @@ export default function Home() {
                     </Row>
                 </Container>
             </div>
+            </div>
+        
+
+
+            {/* <ImageSlider/> */}
             { /* ----------------- banner  section end  ----------------- */}
 
             { /* ----------------- Market section start  ----------------- */}
@@ -604,7 +625,7 @@ export default function Home() {
                                         <h3> Sign Up </h3>
                                         <button className='signup-btn'> Sign Up <EastIcon /> </button>
                                     </div>
-                                    
+
                                     <p className='for-padding'>Sign Up and Claim  500 USDT  token +  200 USDT  coupon + 7500 USDT  Futures Trial Fund</p>
                                 </div>
                             </div>
@@ -617,14 +638,313 @@ export default function Home() {
             { /* ----------------- Crypto Exchange ends   ----------------- */}
 
             { /* ----------------- Product service section start  ----------------- */}
+            <div className="product-service-section">
+                <Container>
+                    <Row>
+                        <div className="pro-ser-heading">
+                            <h2> Explore {siteName} Products & Services</h2>
+                        </div>
+                        <div className="product-service-cards">
+                            <Row>
+                                <Col lg={6}>
+                                    <div className="pro-ser-box">
+                                        <div className="pro-ser-heading">
+                                            <h2> Spot Trading </h2>
+                                            <EastIcon />
+                                        </div>
+                                        <div className="pro-ser-des">
+                                            <p className='pro-des-para'> Trade crypto with our comprehensive set of powerful tools to maximize your profits. </p>
+                                            <div className="pro-ser-img">
+                                                <img src={productImg} alt="" className='img-fluid' />
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col lg={6}>
+                                    <div className="pro-ser-box">
+                                        <div className="pro-ser-heading">
+                                            <h2> Buy Crypto  </h2>
+                                            <EastIcon />
+                                        </div>
+                                        <div className="pro-ser-des">
+                                            <p className='pro-des-para'> Trade crypto with our comprehensive set of powerful tools to maximize your profits. </p>
+                                            <div className="pro-ser-img">
+                                                <img src={productImg} alt="" className='img-fluid' />
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col lg={6}>
+                                    <div className="pro-ser-box">
+                                        <div className="pro-ser-heading">
+                                            <h2> Crypto Derivative  </h2>
+                                            <EastIcon />
+                                        </div>
+                                        <div className="pro-ser-des">
+                                            <p className='pro-des-para'> Trade crypto with our comprehensive set of powerful tools to maximize your profits. </p>
+                                            <div className="pro-ser-img">
+                                                <img src={productImg} alt="" className='img-fluid' />
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col lg={6}>
+                                    <div className="pro-ser-box">
+                                        <div className="pro-ser-heading">
+                                            <h2> {siteName} Earn  </h2>
+                                            <EastIcon />
+                                        </div>
+                                        <div className="pro-ser-des">
+                                            <p className='pro-des-para'> Trade crypto with our comprehensive set of powerful tools to maximize your profits. </p>
+                                            <div className="pro-ser-img">
+                                                <img src={productImg} alt="" className='img-fluid' />
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col lg={6}>
+                                    <div className="pro-ser-box">
+                                        <div className="pro-ser-heading">
+                                            <h2> Trading Bot </h2>
+                                            <EastIcon />
+                                        </div>
+                                        <div className="pro-ser-des">
+                                            <p className='pro-des-para'> Trade crypto with our comprehensive set of powerful tools to maximize your profits. </p>
+                                            <div className="pro-ser-img">
+                                                <img src={productImg} alt="" className='img-fluid' />
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Col>
+                                <Col lg={6}>
+                                    <div className="pro-ser-box">
+                                        <div className="pro-ser-heading">
+                                            <h2> Margin Trading </h2>
+                                            <EastIcon />
+                                        </div>
+                                        <div className="pro-ser-des">
+                                            <p className='pro-des-para'> Trade crypto with our comprehensive set of powerful tools to maximize your profits. </p>
+                                            <div className="pro-ser-img">
+                                                <img src={productImg} alt="" className='img-fluid' />
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </Col>
+
+                            </Row>
+
+                        </div>
+                    </Row>
+                </Container>
+
+            </div>
+            =
+
+            { /* ----------------- Product service section end   ----------------- */}
+
+            { /* ----------------- Safe and trusted section start    ----------------- */}
+            <div className="safe-trusted-main-section">
+            <div className="safe-trusted">
+                <Container>
+                    <div className="safe-sec-heading">
+                        <h1>Your Safe and Trusted Crypto Exchange</h1>
+                    </div>
+                    <div className="safe-learn-more">
+                        <button className='safe-learn-more'> Learn More </button>
+                    </div>
+
+                    <Row>
+                        <Col lg={3}>
+
+                            <div className="safe-cards">
+                                <div className="s-card">
+                                    <div className="s-card-img">
+                                        <img src={safe} alt="" className='img-fluid' />
+                                    </div>
+                                    <div className="s-card-tite">
+                                        <h5> Secure Asset Storage</h5>
+                                    </div>
+                                    <div className="s-card-des">
+                                        <p> Our industry-leading encryption and storage systems ensure that your assets are always safe and secure.</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </Col>
+                        <Col lg={3}>
+
+                            <div className="safe-cards">
+                                <div className="s-card">
+                                    <div className="s-card-img">
+                                        <img src={security1} alt="" className='img-fluid' />
+                                    </div>
+                                    <div className="s-card-tite">
+                                        <h5> Secure Asset Storage</h5>
+                                    </div>
+                                    <div className="s-card-des">
+                                        <p> Our industry-leading encryption and storage systems ensure that your assets are always safe and secure.</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </Col>
+                        <Col lg={3}>
+
+                            <div className="safe-cards">
+                                <div className="s-card">
+                                    <div className="s-card-img">
+                                        <img src={Security2} alt="" className='img-fluid' />
+                                    </div>
+                                    <div className="s-card-tite">
+                                        <h5> Secure Asset Storage</h5>
+                                    </div>
+                                    <div className="s-card-des">
+                                        <p> Our industry-leading encryption and storage systems ensure that your assets are always safe and secure.</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </Col>
+                        <Col lg={3}>
+
+                            <div className="safe-cards">
+                                <div className="s-card">
+                                    <div className="s-card-img">
+                                        <img src={security3} alt="" className='img-fluid' />
+                                    </div>
+                                    <div className="s-card-tite">
+                                        <h5> Secure Asset Storage</h5>
+                                    </div>
+                                    <div className="s-card-des">
+                                        <p> Our industry-leading encryption and storage systems ensure that your assets are always safe and secure.</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+
+            </div>
+
+            </div>
+        
+            { /* ----------------- Safe and trusted section end     ----------------- */}
+
+            { /* ----------------- Safe and trusted Digital Investement      ----------------- */}
+            <div className="digital-investment">
             <Container>
                 <Row>
-                    
+                    <Col lg={6}>
+                        <div className="digttal-heading">
+                            <h2> Make Digital Investment with Ease.</h2>
+                            <p className='digital-des'>
+                                The Future of cryptocurrency exchange is here with Toobit. Instant access, simplified. Trading smoothly, amplified.
+                            </p>
+                        </div>
+                        <div className="digital-scan-code">
+                            <div className="scanner-img">
+                                <img src={qrcode} alt="" />
 
+                            </div>
+                            <div className="scanner-content">
+                                <p> Scan to Download </p>
+                                <p> iOS & Android </p>
+                            </div>
+
+                        </div>
+
+                        <div className="digital-media-app d-flex">
+                            <div className="d-app">
+                                <p> <span> <img src={appstore} alt="" /> </span> App Store </p>
+                            </div>
+                            <div className="d-app">
+                                <p> <span> <img src={playstore} alt="" /> </span> Google play  </p>
+                            </div>
+                            <div className="d-app">
+                                <p> <span> <img src={android} alt="" /></span> APK </p>
+                            </div>
+                            <div className="d-app">
+                                <p> <span> <img src={api} alt="" /></span> API </p>
+                            </div>
+                        </div>
+
+
+                    </Col>
+                    <Col lg={6}>
+                    <div className="rhs-digital-img">
+                        <img src={trading} alt="" className='img-fluid' />
+
+                    </div>
+                    </Col>
                 </Row>
             </Container>
 
-            { /* ----------------- Product service section end   ----------------- */}
+            </div>
+        
+            { /* ----------------- Safe and trusted Digital Investement     ----------------- */}
+
+                    
+            { /* ----------------- Accordian-section-starts     ----------------- */}
+            <section className='accordian-section'>
+          <Container>
+            <Row>
+              <Col>
+                <div className="accordian-main" data-aos="fade-up">
+                  <div className="accordian-heading">
+                    <h1>Frequently Asked questions </h1>
+                  </div>
+                  <div className="accordians">
+                    <Accordion defaultActiveKey="0">
+                      <Accordion.Item eventKey="0" className='accor-item'>
+                        <Accordion.Header>What is blockChain technology?</Accordion.Header>
+                        <Accordion.Body >
+                          It's very simple! Register here. In your personal account, create a wallet where you can store your FOX tokens. Then just send any amount to the displayed address in your office.
+                        </Accordion.Body>
+                      </Accordion.Item>
+                      <Accordion.Item eventKey="1" className='accor-item'>
+                        <Accordion.Header> How its work ?</Accordion.Header>
+                        <Accordion.Body id="accordian-body">
+                          It's very simple! Register here. In your personal account, create a wallet where you can store your FOX tokens. Then just send any amount to the displayed address in your office.
+                        </Accordion.Body>
+                      </Accordion.Item>
+                      <Accordion.Item eventKey="3" className='accor-item'>
+                        <Accordion.Header>Who created bitcoin ?</Accordion.Header>
+                        <Accordion.Body>
+                          It's very simple! Register here. In your personal account, create a wallet where you can store your FOX tokens. Then just send any amount to the displayed address in your office.
+                        </Accordion.Body>
+                      </Accordion.Item>
+                      <Accordion.Item eventKey="4" className='accor-item'>
+                        <Accordion.Header>How does cryptocurrency works ? </Accordion.Header>
+                        <Accordion.Body>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                          aliquip ex ea commodo consequat. Duis aute irure dolor in
+                          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                          culpa qui officia deserunt mollit anim id est laborum.
+                        </Accordion.Body>
+                      </Accordion.Item>
+                    </Accordion>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+            { /* ----------------- Accordian-section-end     ----------------- */}
+
+
+
+
+
 
 
 
